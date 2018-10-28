c39b2696474ab24849d2090fd1172dc6bae2b686
Zaheer <zaheer.aws@gmail.com>
2018-10-28 15:31:36 +0500

    First release

---
+++ index.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<!-- required meta tags -->
+    <meta charset="utf-8">
+
+    <meta name="description" content="Lightning Flashed, Faris Rasul Mirza, Book By Faris Rasul Mirza">
+    <meta name="keywords" content="Lightning Flashed, Faris Rasul, Faris Rasul Mirza, Teenage Writings, Book, Literature, Faris Rasul Book, Faris Rasul Website">
+    
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	
+    <!-- title -->
+    <title>Lightning Flashed | Welcome </title>
+
+    <!-- favicon -->
+    <link rel="shortcut icon" href="img/favicon.ico">
+
+    <!-- google fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700,800,900" rel="stylesheet">
+
+    <!-- fontawesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css">
+
+    <!-- bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+    <!-- Animation CSS -->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+    <!-- Carousel CSS -->
+    <link rel="stylesheet" href="vendor/owlcarousel/owl.carousel.min.css">
+	<link rel="stylesheet" href="vendor/owlcarousel/owl.theme.default.min.css">
+
+    <!-- style CSS -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- responsive CSS -->
+    <link rel="stylesheet" href="css/responsive.css">
+</head>
+<body data-spy="scroll" data-target=".navbar" data-offset="50">
+
+	<!-- home -->
+
+	<section id="home">
+		<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+			<div class="container">
+			<a class="navbar-brand" href="index.html" style="font-family: 'Pacifico', Montserrat;"><img src="img/logo.png"></a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+			</button>
+
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item">
+				<a class="nav-link" href="#home">HOME</a>
+				</li>
+				<li class="nav-item">
+				<a class="nav-link" href="#about">ABOUT</a>
+				</li>
+				<li class="nav-item">
+				<a class="nav-link" href="#testimonial">TESTIMONIALS</a>
+				</li>
+				<li class="nav-item">
+				<a class="nav-link" href="#contact">CONTACT</a>
+				</li>
+			</ul>
+			</div>			
+			</div>
+		</nav>
+
+		
+		<div class="bg-home" data-aos="fade-right">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3 home-left text-center">
+						<img src="img/cover.png" width="90%">
+					</div>
+
+					<div class="col-md-9 home-right text-center">
+						<h1>Lightning Flashed</h1>
+						<h5>BY Faris Rasul Mirza</h5>
+						<p>This is compilation of my writings which have been penned down over a long period of time. The book deals with various subjects and is not particularly centered on a signle topic.</p>
+						<a href="https://www.readings.com.pk/pages/BookDetails.aspx?BookID=779792" target="_blank"><button class="btn btn-danger btn-home" data-aos="flip-left">GET YOUR COPY</button></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- about -->
+
+	<section id="about">
+		<div class="container" data-aos="fade-up">
+			<div class="content text-center">
+				<h2>About the Author</h2>
+				<hr class="h-rule">
+				<div class="about-padding"></div>
+				<img src="img/author.jpg" alt="author" width="18%">
+				<p>Faris Rasul Mirza is an undergraduate student
+				<br><br>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<div class="social">
+					<a href="https://facebook.com" target="_blank"><i class="fab fa-facebook-f fa-2x"></i></a>
+					<a href="https://twitter.com" target="_blank"><i class="fab fa-twitter fa-2x"></i></a>
+					<a href="https://goodreads.com" target="_blank"><i class="fab fa-goodreads-g fa-2x"></i></a>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- testimonial -->
+
+	<section id="testimonial">
+		<div class="container" data-aos="fade-up">
+			<div class="content text-center">
+				<h2>Testimonials</h2>
+				<hr class="h-rule">
+			</div>
+			<div class="owl-carousel owl-theme text-center">
+			    
+			    <div class="item">
+			    	<blockquote><p>It is heartening to know that, as evinced by quite a few pieces, Faris has an eye on the Holy Quran, the epitome of wisdom and knowledge for all times to come and thus he is duly "enlightened" with regard to the perennial philsophical questions.</p></blockquote>
+			    	<h4>Dr. Absar Ahmed</h4>
+			    	<h5>Professor</h5>
+			    </div>
+
+			    <div class="item">
+			    	<blockquote><p>The written memories of an intermediate-studying teenager, is such a document that even if you haven't met Faris Rasul, it will seem to you a writing of a sixty-year-old retired professor, who is fixated with Islam, Pakistan and Eastern Civilization.</p></blockquote>
+			    	<h4>Dr. Ishtiaq Ahmad Gondal</h4>
+			    	<h5>University of Punjab</h5>
+			    </div>
+
+			    <div class="item">
+			    	<blockquote><p>May all our youth be as profoundly moved, as Faris is about issues of deprivation and political apathy- for it is these feelings of unease in our youth that may collectively lead to movements for equality, progress and peace in the future. I wish him all the best for his future endeavors.</p></blockquote>
+			    	<h4>Dr. Sara Rizvi</h4>
+			    	<h5>Forman Christian College</h5>
+			    </div>
+
+			    <div class="item">
+			    	<blockquote><p>In short, very well-organized structure, logically sequenced ideas, high frequency lexis and the use of strong convincing arguments are the real artistic beauties which have fascinated me from the start to the finish. You doubtlessly excel yourself among your contemporary writers.</p></blockquote>
+			    	<h4>Prof. Zulfiqar Ali</h4>
+			    	<h5>King Abdul Aziz University</h5>
+			    </div>
+			</div>
+		</div>
+	</section>
+
+	<!-- contact -->
+
+	<section id="contact">
+		<div class="container" data-aos="fade-right">
+			<div class="content text-center">
+				<h2>Get in touch</h2>
+				<hr class="h-rule">
+				<div class="contact-detail">
+					<img src="img/logo.png">
+					<p>Drop me an email at farisrasul@yahoo.com</p>
+					<p>I'd love to hear from you. Wanna ask something? <br>Make a comment or just say 'hi'. I will get back to you as soon as I can.</p>
+					<a href="mailto: farisrasul@yahoo.com"><button class="btn btn-contact">Drop an Email</button></a>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- footer -->
+
+	<div id="footer">
+		<p>&copy; Lightning Flashed 2018. Designed By: <a href="https://twitter.com" target="_blank">HAMXA</a></p>	
+	</div>
+
+
+
+
+	<!-- jQuery CDN -->
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+
+	<!-- bootstrap JS -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+	<!-- animation JS -->
+	<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+	    <script>
+  			AOS.init();
+		</script>
+
+	<!-- carousel JS -->
+	<script src="vendor/owlcarousel/owl.carousel.min.js"></script>
+		
+	<!-- cutom JS -->
+    <script src="js/custom.js"></script>
+    <script type="text/javascript">
+		$('.nav-link').on('click',function() {
+		$('.navbar-collapse').collapse('hide');
+		});
+	</script>
+
+</body>
+</html>
